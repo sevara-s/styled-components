93c06fdef8dfb5af397d0a9a9c548af06f046ea5
sevara-s <nematullayevasevara6@gmail.com>
2025-02-14 20:38:06 +0500

    styled components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from "react";
+import { Container } from "./components/styles.js";
+
+function App() {
+  return (
+    <>
+      <Container>
+        <h1>this in container</h1>
+      </Container>
+    </>
+  );
+}
+
+export default App;
